96d24af06691549aee8e695d9fe3b6e2b5f16891
Zach Lockett-Streiff <zachls17@gmail.com>
2018-06-15 20:05:29 -0500

    Initial Commit

---
+++ DanceEventManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DanceEventManager
+//
+//  Created by Zach Lockett-Streiff on 6/15/18.
+//  Copyright © 2018 Swingaroo2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
