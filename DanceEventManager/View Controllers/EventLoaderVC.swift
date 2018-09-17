dd7c6181f0c63c0bd64db6bd94b37d7b6360f9b8
Zach Lockett-Streiff <zachls17@gmail.com>
2018-09-16 21:23:54 -0500

    Populating date picker text fields with selected dates

--- DanceEventManager/View Controllers/EventLoaderVC.swift
+++ DanceEventManager/View Controllers/EventLoaderVC.swift
@@ -11,16 +11,17 @@ import UIKit
 class EventLoaderVC: UIViewController {
 
     @IBOutlet weak var eventPickerTextField: UITextField!
+    @IBOutlet weak var startDatePicker: UITextField!
+    @IBOutlet weak var endDatePicker: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setUpDatePickers()
         self.loadEvents()
-        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func loadEvents() {
@@ -29,15 +30,62 @@ class EventLoaderVC: UIViewController {
         events.append("New Event") // Default entry
         self.eventPickerTextField.loadDropdownData(data: events)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func setUpDatePickers() {
+        self.configureDatePicker(self.startDatePicker)
+        self.configureDatePicker(self.endDatePicker)
     }
-    */
+    
+    func configureDatePicker(_ textField:UITextField) {
+        let datePickerView:UIDatePicker = UIDatePicker()
+        datePickerView.datePickerMode = .date
+        datePickerView.minimumDate = Date()
+        datePickerView.addTarget(self, action: #selector(EventLoaderVC.datePickerValueChanged(_:)), for: .valueChanged)
+        textField.inputView = datePickerView
+        self.addToolbar(textField)
+    }
+    
+    func addToolbar(_ textField:UITextField) {
+        let toolBar = UIToolbar()
+        toolBar.barStyle = .default
+        toolBar.isTranslucent = true
+        toolBar.tintColor = UIColor(red: 92/255, green: 216/255, blue: 255/255, alpha: 1)
+        toolBar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(EventLoaderVC.datePickerDoneButtonPressed(_:)))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        toolBar.setItems([spaceButton, doneButton, spaceButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        textField.inputAccessoryView = toolBar
+    }
 
+    @objc func datePickerValueChanged(_ sender:UIDatePicker) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM dd yyyy"
+        let dateStr = formatter.string(from: sender.date)
+        
+        if self.startDatePicker.isFirstResponder {
+            self.startDatePicker.text = dateStr
+        } else if self.endDatePicker.isFirstResponder {
+            self.endDatePicker.text = dateStr
+        }
+        
+    }
+    
+    @objc func datePickerDoneButtonPressed(_ sender:Any) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM dd yyyy"
+        if self.startDatePicker.isFirstResponder {
+            let dateStr = formatter.string(from: (self.startDatePicker.inputView as! UIDatePicker).date)
+            self.startDatePicker.text = dateStr
+        } else if self.endDatePicker.isFirstResponder {
+            let dateStr = formatter.string(from: (self.endDatePicker.inputView as! UIDatePicker).date)
+            self.endDatePicker.text = dateStr
+        }
+
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func showStartDatePicker(_ sender: UITextField) {}
+    @IBAction func showEndDatePicker(_ sender: UITextField) {}
 }
